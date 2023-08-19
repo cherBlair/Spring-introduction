3f6e770467a30451f5b0598bbae57f920c3ebe53
blair <cher.blair.h@gmail.com>
2023-08-19 19:05:02 +0900

    member domain, repository

---
+++ src/main/java/blair/hellospring/repository/MemberRepository.java
@@ -0,0 +1,15 @@
+package blair.hellospring.repository;
+
+import blair.hellospring.domain.Member;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface MemberRepository {
+
+    Member save(Member member);
+    Optional<Member> findById(Long id); // Optional: java8 -> null 반환을 감싸서 반환하는 형식
+    Optional<Member> findByName(String name);
+    List<Member> findAll();
+}
+
