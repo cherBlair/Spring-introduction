6ea36fdd4965fa3a919a432fad33ce4f2e25a95f
blair <cher.blair.h@gmail.com>
2023-08-24 22:32:29 +0900

    members / register

---
+++ src/main/java/blair/hellospring/controller/MemberForm.java
@@ -0,0 +1,13 @@
+package blair.hellospring.controller;
+
+public class MemberForm {
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    private String name;
+}
