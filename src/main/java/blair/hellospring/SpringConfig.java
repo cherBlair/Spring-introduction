44f40d72384a562239a9eb7f44e783dc88efce4c
blair <cher.blair.h@gmail.com>
2023-08-25 21:23:38 +0900

    JDBC / old fashioned

--- src/main/java/blair/hellospring/SpringConfig.java
+++ src/main/java/blair/hellospring/SpringConfig.java
@@ -1,13 +1,23 @@
 package blair.hellospring;
 
+import blair.hellospring.repository.JdbcMemberRepository;
 import blair.hellospring.repository.MemberRepository;
-import blair.hellospring.repository.MemoryMemberRepository;
 import blair.hellospring.service.MemberService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import javax.sql.DataSource;
+
 @Configuration
 public class SpringConfig {
+
+    private DataSource dataSource;
+
+    @Autowired
+    public SpringConfig(DataSource dataSource) {
+        this.dataSource = dataSource;
+    }
 
     @Bean
     public MemberService memberService() {
@@ -16,6 +26,8 @@ public class SpringConfig {
 
     @Bean
     public MemberRepository memberRepository() {
-        return new MemoryMemberRepository();
+
+        //return new MemoryMemberRepository();
+        return new JdbcMemberRepository(dataSource);
     }
 }
