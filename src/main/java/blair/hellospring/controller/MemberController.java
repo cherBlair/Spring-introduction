cee50bba434773a92c532c09df2f21f7bf8377b5
blair <cher.blair.h@gmail.com>
2023-08-25 19:30:23 +0900

    member / list

--- src/main/java/blair/hellospring/controller/MemberController.java
+++ src/main/java/blair/hellospring/controller/MemberController.java
@@ -4,8 +4,11 @@ import blair.hellospring.domain.Member;
 import blair.hellospring.service.MemberService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+
+import java.util.List;
 
 @Controller
 // @Controller
@@ -37,5 +40,13 @@ public class MemberController {
 
         return "redirect:/";
     }
+
+    @GetMapping(value = "/members")
+    public String list(Model model) {
+        List<Member> members = memberService.findMembers();
+        model.addAttribute("members", members);
+        return "members/memberList";
+    }
+
 
 }
